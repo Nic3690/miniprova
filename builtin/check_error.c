6de242910b292771773e610c77d639486aae2de4
Nicoletta Furlani <nfurlani@MacBook-Air-di-Nicoletta.local>
2024-05-16 00:01:20 +0200

    fixed red

--- builtin/check_error.c
+++ builtin/check_error.c
@@ -6,7 +6,7 @@
 /*   By: nfurlani <nfurlani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 21:10:09 by nfurlani          #+#    #+#             */
-/*   Updated: 2024/05/15 21:40:42 by nfurlani         ###   ########.fr       */
+/*   Updated: 2024/05/16 00:00:33 by nfurlani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,23 @@
 
 int	check_token_error(t_lexer **lexer)
 {
+	t_lexer	*head;
+
+	head = *lexer;
 	if (ft_strcmp((*lexer)->token, "|") == 0)
 		return (printf("syntax error near unexpected token '|'\n"));
 	if ((*lexer)->token && !(*lexer)->next)
 		return (printf("syntax error near unexpected token 'newline'\n"));
-	if ((*lexer)->next && (*lexer)->token && (*lexer)->next->token)
-		return (printf("syntax error near unexpected token '%s'\n", (*lexer)->token));
+	while (*lexer)
+	{
+		if ((*lexer)->next && (*lexer)->token && (*lexer)->next->token)
+		{
+			printf("syntax error near unexpected token '%s'\n", (*lexer)->token);
+			*lexer = head;
+			return (1);
+		}
+		*lexer = (*lexer)->next;
+	}
+	*lexer = head;
 	return (0);
 }
